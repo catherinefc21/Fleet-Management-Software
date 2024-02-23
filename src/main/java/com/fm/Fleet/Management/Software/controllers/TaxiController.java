c5b0993ecf5c5cb7abc10dd8673200f899de4be4
catherinefc21 <129704819+catherinefc21@users.noreply.github.com>
2024-02-23 16:25:33 -0300

    controller changes

--- src/main/java/com/fm/Fleet/Management/Software/controllers/TaxiController.java
+++ src/main/java/com/fm/Fleet/Management/Software/controllers/TaxiController.java
@@ -2,9 +2,12 @@ package com.fm.Fleet.Management.Software.controllers;
 
 import com.fm.Fleet.Management.Software.models.TaxiModel;
 import com.fm.Fleet.Management.Software.services.TaxiService;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.data.domain.Page;
@@ -19,7 +22,13 @@ public class TaxiController {
 
     // Endpoint para obtener la lista de taxis con paginación
     @GetMapping
-       public ResponseEntity<Page<TaxiModel>> getTaxis(Pageable pageable) {
+    public ResponseEntity<Page<TaxiModel>> getTaxis(
+            @Parameter(description = "Número de la página a recuperar")
+            @RequestParam(defaultValue = "0") int page,
+            @Parameter(description = "Cantidad de registros por página")
+            @RequestParam(defaultValue = "10") int size) {
+
+        Pageable pageable = PageRequest.of(page, size);
         return ResponseEntity.ok(taxiService.getTaxis(pageable));
     }
 }
